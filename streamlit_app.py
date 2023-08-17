ce5456023ab3b2c4d4d50bae615028cc87ecf226
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-17 14:19:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,30 +60,35 @@ if uploaded_file is not None:
   with open("converted.png", "wb") as f:
     f.write(output_buffer.getvalue())
 
-  # Convert to Twitter Social Preview
-  st.subheader("Twitter Social Preview")
-  twitter_img = img2twitter("converted.png")
-  twitter_img.save("twitter.png")
-  st.image(twitter_img, use_column_width=True)
+  col1, col2 = st.columns(2)
+
   
-  with open("twitter.png", "rb") as file:
-    btn = st.download_button(
-                label="Download Twitter social preview image",
-                data=file,
-                file_name=f"{uploaded_file.name.rstrip('.svg')}_twitter.png",
-                mime="image/png"
-              )
+  # Convert to Twitter Social Preview
+  with col1:
+    st.subheader("Twitter Social Preview")
+    twitter_img = img2twitter("converted.png")
+    twitter_img.save("twitter.png")
+    st.image(twitter_img, use_column_width=True)
+    
+    with open("twitter.png", "rb") as file:
+      btn = st.download_button(
+                  label="Download Twitter social preview image",
+                  data=file,
+                  file_name=f"{uploaded_file.name.rstrip('.svg')}_twitter.png",
+                  mime="image/png"
+                )
 
   # Convert to LinkedIn Social Preview
-  st.subheader("LinkedIn Social Preview")
-  linkedin_img = img2linkedin("converted.png")
-  linkedin_img.save("linkedin.png")
-  st.image(linkedin_img, use_column_width=True)
-  
-  with open("linkedin.png", "rb") as file:
-    btn = st.download_button(
-                label="Download LinkedIn social preview image",
-                data=file,
-                file_name=f"{uploaded_file.name.rstrip('.svg')}_linkedin.png",
-                mime="image/png"
-              )
+  with col2:
+    st.subheader("LinkedIn Social Preview")
+    linkedin_img = img2linkedin("converted.png")
+    linkedin_img.save("linkedin.png")
+    st.image(linkedin_img, use_column_width=True)
+    
+    with open("linkedin.png", "rb") as file:
+      btn = st.download_button(
+                  label="Download LinkedIn social preview image",
+                  data=file,
+                  file_name=f"{uploaded_file.name.rstrip('.svg')}_linkedin.png",
+                  mime="image/png"
+                )
